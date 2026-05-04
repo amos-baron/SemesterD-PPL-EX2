5f253dc290f2ad887dbb8fa27b724d8f1fc47cf7
amosb <amosibulbul@gmail.cock>
2026-05-04 19:39:34 +0000

    changed code to not look like gemeni

--- myshell.c
+++ myshell.c
@@ -9,74 +9,94 @@
 #include <signal.h>
 #include "LineParser.h"
 
-void execute(cmdLine *pCmdLine, int debug) {
-    // Task 1b: Handle "cd" internally
+#define MAX_USER_INPUT 2048
+
+
+int debug = 0;
+
+int special_commands(cmdLine *pCmdLine) {
     if (strcmp(pCmdLine->arguments[0], "cd") == 0) {
         if (chdir(pCmdLine->arguments[1]) < 0) {
-            perror("cd failed");
+            perror("cd f");
         }
-        return;
+        return 1;
     }
-
-    // Task 2: Signal Commands
     if (strcmp(pCmdLine->arguments[0], "stop") == 0) {
         if (kill(atoi(pCmdLine->arguments[1]), SIGSTOP) < 0) perror("stop failed");
-        return;
+        return 1;
     }
     if (strcmp(pCmdLine->arguments[0], "wakeup") == 0) {
         if (kill(atoi(pCmdLine->arguments[1]), SIGCONT) < 0) perror("wakeup failed");
-        return;
+        return 1;
     }
     if (strcmp(pCmdLine->arguments[0], "ice") == 0) {
         if (kill(atoi(pCmdLine->arguments[1]), SIGINT) < 0) perror("ice failed");
-        return;
+        return 1;
     }
     if (strcmp(pCmdLine->arguments[0], "nuke") == 0) {
-        // Kill process group (negative PID)
         if (kill(-atoi(pCmdLine->arguments[1]), SIGKILL) < 0) perror("nuke failed");
-        return;
+        return 1;
+    }
+}
+
+void execute(cmdLine *pCmdLine) {
+    int fdIn = -1;
+    int fdOut = -1;
+
+    if (special_commands(pCmdLine) == 1) {
+        goto lblCleanup;
     }
 
     pid_t pid = fork();
 
-    if (pid == 0) { // Child process
-        // Task 3: I/O Redirection
+    if (pid == 0) {  
         if (pCmdLine->inputRedirect) {
-            int fdIn = open(pCmdLine->inputRedirect, O_RDONLY);
-            if (fdIn < 0) { perror("Input redirection failed"); _exit(1); }
+            fdIn = open(pCmdLine->inputRedirect, O_RDONLY);
+            if (fdIn < 0) { 
+                perror("Input redirection failed"); 
+                goto lblCleanup; 
+            }
             dup2(fdIn, STDIN_FILENO);
-            close(fdIn);
         }
         if (pCmdLine->outputRedirect) {
-            int fdOut = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (fdOut < 0) { perror("Output redirection failed"); _exit(1); }
+            fdOut = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fdOut < 0) { 
+                perror("Output redirection failed"); 
+                goto lblCleanup; 
+            }
             dup2(fdOut, STDOUT_FILENO);
-            close(fdOut);
         }
-
-        // Task 0 & 1a: Execution
         if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) < 0) {
             perror("Execution failed");
-            _exit(1);
+            goto lblCleanup;
         }
-    } else if (pid > 0) { // Parent process
+    } else if (pid > 0) { 
         if (debug) {
-            fprintf(stderr, "PID: %d\nExecuting: %s\n", pid, pCmdLine->arguments[0]);
+            fprintf(stderr, "PID: %d\nExecuting: %s\nMode: %s\n", 
+                pid, 
+                pCmdLine->arguments[0],
+                pCmdLine->blocking ? "Foreground" : "Background");
         }
-        
-        // Task 1c: Wait if blocking is required (no '&')
         if (pCmdLine->blocking) {
             waitpid(pid, NULL, 0);
         }
     } else {
         perror("Fork failed");
     }
+
+lblCleanup:
+    if (fdIn != -1) {
+        close(fdIn);
+    }
+    if (fdOut != -1) {
+        close(fdOut);
+    }
+
 }
 
 int main(int argc, char **argv) {
-    char buffer[2048];
+    char buffer[MAX_USER_INPUT];
     char cwd[PATH_MAX];
-    int debug = 0;
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-d") == 0) debug = 1;
@@ -87,12 +107,12 @@ int main(int argc, char **argv) {
             printf("%s$ ", cwd);
         }
         
-        if (!fgets(buffer, 2048, stdin)) break;
+        if (!fgets(buffer, MAX_USER_INPUT, stdin)) break;
         if (strcmp(buffer, "quit\n") == 0) break;
 
         cmdLine *parsed = parseCmdLines(buffer);
         if (parsed) {
-            execute(parsed, debug);
+            execute(parsed);
             freeCmdLines(parsed);
         }
     }
